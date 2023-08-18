4a7e46538e56a70bb83588a6ff9cc4531794c556
HsiaoTing Chen <fatfattim@gmail.com>
2023-08-18 10:44:37 +0800

    Demo the livedata & shareddata & SharedFlow

--- app/src/main/java/com/tim/taiwanstock/ui/home/HomeFragment.kt
+++ app/src/main/java/com/tim/taiwanstock/ui/home/HomeFragment.kt
@@ -1,13 +1,17 @@
 package com.tim.taiwanstock.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.tim.taiwanstock.databinding.FragmentHomeBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
 
@@ -16,23 +20,54 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-
+    private lateinit var viewModel: HomeViewModel
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
+        viewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
+        viewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+        viewModel.liveData.observe(viewLifecycleOwner) {
+            Log.d("TimT", "observe live data: $it")
+        }
+
+        lifecycleScope.launch(Dispatchers.Main) {
+            viewModel.stateFlow.collect { state ->
+                Log.d("TimT", "observe state flow: $state")
+            }
+        }
+
+        lifecycleScope.launch(Dispatchers.Main) {
+
+            viewModel.dataSharedFlow.collect{ state ->
+                Log.d("TimT", "observe data shared flow: $state")
+            }
+        }
+
+
+
+        binding.myButton.setOnClickListener {
+            viewModel.updateLiveDataAndStateFlow()
+        }
         return root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        printLiveDataAndFlowData()
+    }
+    fun printLiveDataAndFlowData() {
+        Log.d("TimT", "live data: ${viewModel.liveData.value}")
+        Log.d("TimT", "flow data: ${viewModel.stateFlow.value}")
+        Log.d("TimT", "test data: ${viewModel.test}")
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/tim/taiwanstock/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/tim/taiwanstock/ui/home/HomeViewModel.kt
@@ -1,8 +1,16 @@
 package com.tim.taiwanstock.ui.home
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel() {
 
@@ -10,4 +18,31 @@ class HomeViewModel : ViewModel() {
         value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
+
+
+    private val _liveData: MutableLiveData<String>  = MutableLiveData<String>()
+    val liveData: LiveData<String>
+        get() = _liveData
+
+    private val _stateFlow: MutableStateFlow<String> = MutableStateFlow("")
+    val stateFlow: StateFlow<String> = _stateFlow.asStateFlow()
+    private val _dataSharedFlow = MutableSharedFlow<String>()
+
+    val dataSharedFlow: SharedFlow<String> = _dataSharedFlow
+    var test: String = "init"
+
+    fun updateLiveDataAndStateFlow() {
+        _liveData.value = "updateLiveData"
+        _stateFlow.value = "updateStateFlow"
+        test = "change"
+        viewModelScope.launch {
+            _dataSharedFlow.emit("updateStateFlow")
+        }
+    }
+    override fun onCleared() {
+        super.onCleared()
+        Log.d("TimT", "onCleared: ")
+        _liveData.value = "onCleared"
+    }
+
 }
